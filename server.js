e4743f4eda4b101edee16ea3dc2615c971cd9ee6
Glitch (hello-express) <none>
2019-07-18 02:52:12 +0000

    🏞⛸ Checkpoint ./public/client.js:72439/6 ./package.json:72439/1281 ./server.js:72439/869

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 // init project
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 // we've started you off with Express, 
@@ -21,8 +22,32 @@ const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-setInterval(getStuff, 10000);
+// Bus stuff
+
+let services = ['KPL', 60, 220, 210, 226, 236, 230, 250, 260];
+let url = 'https://www.metlink.org.nz/api/v1/ServiceLocation/';
 
 function getStuff(){
   console.log('getting Stuff....');
+  
+  services.map(function(serviceId){
+    console.log(url + serviceId);
+    
+    axios.get(url + serviceId)
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+    
+  });
 }
+
+getStuff();
+setInterval(getStuff, 10000);
