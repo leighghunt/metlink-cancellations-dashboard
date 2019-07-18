12bcf8dc31e39234fbc347938176c3a5da9a0591
Glitch (hello-express) <none>
2019-07-18 03:03:15 +0000

    🌏👖 Checkpoint ./server.js:72439/6546

--- server.js
+++ server.js
@@ -24,8 +24,9 @@ const listener = app.listen(process.env.PORT, function() {
 
 // Bus stuff
 
-let services = ['KPL', 60, 220, 210, 226, 236, 230, 250, 260];
+let services = ['KPL'] ;//, 60, 220, 210, 226, 236, 230, 250, 260];
 let url = 'https://www.metlink.org.nz/api/v1/ServiceLocation/';
+
 
 function getStuff(){
   console.log('getting Stuff....');
@@ -36,18 +37,227 @@ function getStuff(){
     axios.get(url + serviceId)
     .then(function (response) {
       // handle success
-      console.log(response);
+      // console.log(response.data);
+      
+      response.data.Services.forEach(function(service){
+        console.log(service.ServiceID + ': ' + service.VehicleRef);
+      });
+      
     })
     .catch(function (error) {
       // handle error
       console.log(error);
     })
-    .finally(function () {
-      // always executed
-    });
     
   });
 }
 
-getStuff();
-setInterval(getStuff, 10000);
+//setTimeout(getStuff, 10000); // Avoid firing immediately so we don't balst the API and get throttled.
+//getStuff();
+//setInterval(getStuff, 60000);
+
+const KPL_JSON = {
+  "LastModified": "2019-07-18T15:00:09+12:00",
+  "Services": [
+    {
+      "RecordedAtTime": "2019-07-18T14:59:25+12:00",
+      "VehicleRef": "4157",
+      "ServiceID": "KPL",
+      "HasStarted": true,
+      "DepartureTime": "2019-07-18T14:13:00+12:00",
+      "OriginStopID": "WELL",
+      "OriginStopName": "WgtnStn",
+      "DestinationStopID": "WAIK",
+      "DestinationStopName": "WaikanaeStn",
+      "Direction": "Outbound",
+      "Bearing": "24",
+      "BehindSchedule": true,
+      "VehicleFeature": null,
+      "DelaySeconds": 81,
+      "Lat": "-40.9897385",
+      "Long": "174.9504089",
+      "Service": {
+        "Code": "KPL",
+        "TrimmedCode": "KPL",
+        "Name": "Kapiti Line (Wellington - Waikanae)",
+        "Mode": "Train",
+        "Link": "/timetables/train/KPL"
+      }
+    },
+    {
+      "RecordedAtTime": "2019-07-18T14:59:25+12:00",
+      "VehicleRef": "4225",
+      "ServiceID": "KPL",
+      "HasStarted": true,
+      "DepartureTime": "2019-07-18T14:40:00+12:00",
+      "OriginStopID": "WAIK",
+      "OriginStopName": "WaikanaeStn",
+      "DestinationStopID": "WELL",
+      "DestinationStopName": "WgtnStn",
+      "Direction": "Inbound",
+      "Bearing": "210",
+      "BehindSchedule": true,
+      "VehicleFeature": null,
+      "DelaySeconds": 56,
+      "Lat": "-41.0158081",
+      "Long": "174.9170685",
+      "Service": {
+        "Code": "KPL",
+        "TrimmedCode": "KPL",
+        "Name": "Kapiti Line (Waikanae - Wellington)",
+        "Mode": "Train",
+        "Link": "/timetables/train/KPL"
+      }
+    },
+    {
+      "RecordedAtTime": "2019-07-18T14:59:25+12:00",
+      "VehicleRef": "4228",
+      "ServiceID": "KPL",
+      "HasStarted": false,
+      "DepartureTime": "2019-07-18T15:02:00+12:00",
+      "OriginStopID": "WAIK",
+      "OriginStopName": "WaikanaeStn",
+      "DestinationStopID": "WELL",
+      "DestinationStopName": "WgtnStn",
+      "Direction": "Inbound",
+      "Bearing": "336",
+      "BehindSchedule": false,
+      "VehicleFeature": null,
+      "DelaySeconds": 0,
+      "Lat": "-40.8765984",
+      "Long": "175.0662384",
+      "Service": {
+        "Code": "KPL",
+        "TrimmedCode": "KPL",
+        "Name": "Kapiti Line (Waikanae - Wellington)",
+        "Mode": "Train",
+        "Link": "/timetables/train/KPL"
+      }
+    },
+    {
+      "RecordedAtTime": "2019-07-18T14:59:25+12:00",
+      "VehicleRef": "4231",
+      "ServiceID": "KPL",
+      "HasStarted": true,
+      "DepartureTime": "2019-07-18T14:00:00+12:00",
+      "OriginStopID": "WAIK",
+      "OriginStopName": "WaikanaeStn",
+      "DestinationStopID": "WELL",
+      "DestinationStopName": "WgtnStn",
+      "Direction": "Inbound",
+      "Bearing": "174",
+      "BehindSchedule": true,
+      "VehicleFeature": null,
+      "DelaySeconds": 25,
+      "Lat": "-41.2746086",
+      "Long": "174.7841187",
+      "Service": {
+        "Code": "KPL",
+        "TrimmedCode": "KPL",
+        "Name": "Kapiti Line (Waikanae - Wellington)",
+        "Mode": "Train",
+        "Link": "/timetables/train/KPL"
+      }
+    },
+    {
+      "RecordedAtTime": "2019-07-18T14:59:25+12:00",
+      "VehicleRef": "4237",
+      "ServiceID": "KPL",
+      "HasStarted": false,
+      "DepartureTime": "2019-07-18T15:01:00+12:00",
+      "OriginStopID": "WELL",
+      "OriginStopName": "WgtnStn",
+      "DestinationStopID": "PORI2",
+      "DestinationStopName": "PoriruaStn",
+      "Direction": "Outbound",
+      "Bearing": "336",
+      "BehindSchedule": false,
+      "VehicleFeature": null,
+      "DelaySeconds": 0,
+      "Lat": "-41.2777481",
+      "Long": "174.7813416",
+      "Service": {
+        "Code": "KPL",
+        "TrimmedCode": "KPL",
+        "Name": "Kapiti Line (Wellington - Waikanae)",
+        "Mode": "Train",
+        "Link": "/timetables/train/KPL"
+      }
+    },
+    {
+      "RecordedAtTime": "2019-07-18T14:59:25+12:00",
+      "VehicleRef": "4266",
+      "ServiceID": "KPL",
+      "HasStarted": true,
+      "DepartureTime": "2019-07-18T14:20:00+12:00",
+      "OriginStopID": "WAIK",
+      "OriginStopName": "WaikanaeStn",
+      "DestinationStopID": "WELL",
+      "DestinationStopName": "WgtnStn",
+      "Direction": "Inbound",
+      "Bearing": "186",
+      "BehindSchedule": false,
+      "VehicleFeature": null,
+      "DelaySeconds": -35,
+      "Lat": "-41.1474113",
+      "Long": "174.8428192",
+      "Service": {
+        "Code": "KPL",
+        "TrimmedCode": "KPL",
+        "Name": "Kapiti Line (Waikanae - Wellington)",
+        "Mode": "Train",
+        "Link": "/timetables/train/KPL"
+      }
+    },
+    {
+      "RecordedAtTime": "2019-07-18T14:59:25+12:00",
+      "VehicleRef": "4288",
+      "ServiceID": "KPL",
+      "HasStarted": true,
+      "DepartureTime": "2019-07-18T14:53:00+12:00",
+      "OriginStopID": "WELL",
+      "OriginStopName": "WgtnStn",
+      "DestinationStopID": "WAIK",
+      "DestinationStopName": "WaikanaeStn",
+      "Direction": "Outbound",
+      "Bearing": "348",
+      "BehindSchedule": false,
+      "VehicleFeature": null,
+      "DelaySeconds": 0,
+      "Lat": "-41.2520447",
+      "Long": "174.8046417",
+      "Service": {
+        "Code": "KPL",
+        "TrimmedCode": "KPL",
+        "Name": "Kapiti Line (Wellington - Waikanae)",
+        "Mode": "Train",
+        "Link": "/timetables/train/KPL"
+      }
+    },
+    {
+      "RecordedAtTime": "2019-07-18T14:59:25+12:00",
+      "VehicleRef": "4293",
+      "ServiceID": "KPL",
+      "HasStarted": true,
+      "DepartureTime": "2019-07-18T14:33:00+12:00",
+      "OriginStopID": "WELL",
+      "OriginStopName": "WgtnStn",
+      "DestinationStopID": "WAIK",
+      "DestinationStopName": "WaikanaeStn",
+      "Direction": "Outbound",
+      "Bearing": "354",
+      "BehindSchedule": true,
+      "VehicleFeature": null,
+      "DelaySeconds": 112,
+      "Lat": "-41.1112137",
+      "Long": "174.8609009",
+      "Service": {
+        "Code": "KPL",
+        "TrimmedCode": "KPL",
+        "Name": "Kapiti Line (Wellington - Waikanae)",
+        "Mode": "Train",
+        "Link": "/timetables/train/KPL"
+      }
+    }
+  ]
+};
