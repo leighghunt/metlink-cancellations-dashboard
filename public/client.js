9fc45ac94ec236061076eddd598f0d9b7b79e358
Glitch (hello-express) <none>
2019-07-31 03:56:37 +0000

    🎮📺 Checkpoint ./public/client.js:72439/6254 ./views/index.html:72439/54

--- public/client.js
+++ public/client.js
@@ -1,170 +1,19 @@
-// client-side js
-// run by the browser each time your view template is loaded
-
-console.log('hello world :o');
-
-var porirua = [-41.135461, 174.839714]
-var poriruaCollege = [-41.141636, 174.873872]
-
-console.log(L); 
-var L = window.L;
-var map = L.map('map').setView(porirua, 11);
-
-var tileLayerOSM = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-
-// Available Thundermap themes:
-var theme = 'transport'; // Others: cycle, landscape, outdoors, transport-dark, spinal-map, pioneer, mobile-atlas, neighbourhood
-// theme = 'cycle';
-theme = 'transport-dark';
-// theme = 'outdoors;
-// theme = 'neighbourhood';
-
-var tileLayerThunderforest = 'https://{s}.tile.thunderforest.com/' + theme + '/{z}/{x}/{y}{r}.png?apikey=7dd44766c60140818b8816a0d8521fc2';
-
-var tileLayerUrl = tileLayerThunderforest;
-
-L.tileLayer(tileLayerUrl, {
-  opacity: 0.3,
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker(poriruaCollege).addTo(map)
-    .bindPopup('Porirua College')
-    // .openPopup();
-
-map.locate({setView: false, maxZoom: 16});
-
-function onLocationFound(e) {
-    var radius = e.accuracy;
-
-    // L.marker(e.latlng).addTo(map)
-
-    L.circle(e.latlng, radius).addTo(map)
-          .bindPopup("You are within " + radius + " meters from this point");
-
-}
-
-function onLocationError(e) {
-    alert(e.message);
-}
-
-map.on('locationerror', onLocationError);
-map.on('locationfound', onLocationFound);
-
-
-console.log('About to connect to sockets');
-console.log(window.location.hostname);
-var io = window.io;
-
-var socket = io.connect(window.location.hostname);
-var vehicles = {};
-var markers= {};
-var trails = {};
-console.log(vehicles);
-
-// a helper function to call when our request for dreams is done
-const getVehiclesListener = function() {
-  // parse our response to convert to JSON
-  console.log('getVehiclesListener')
-  let latestVehicles = JSON.parse(this.responseText);
-
-  // iterate through every dream and add it to our page
-  for(var VehicleRef in latestVehicles){
-    handleVehicleData(latestVehicles[VehicleRef]);
+var x = document.getElementById("demo");
+function getLocation() {
+  console.log('getLocation')
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
 
-// request the dreams from our app's sqlite database
-const vehiclesRequest = new XMLHttpRequest();
-vehiclesRequest.onload = getVehiclesListener;
-vehiclesRequest.open('get', '/latest');
-vehiclesRequest.send();
-
-
-socket.on('location', function (data) {
-  console.log(data);
-  handleVehicleData(data);
-});
-          
-function handleVehicleData(data){
-
-  if(vehicles[data.VehicleRef] && vehicles[data.VehicleRef].RecordedAtTime == data.RecordedAtTime){
-    console.log('no update');
-    return;
-  }
-  
-  vehicles[data.VehicleRef] = data;
-  let colour = 'green';
-    let fillColour = '#3f0';
-    if(data.DelaySeconds>60){
-      colour = 'yellow';
-      fillColour = '#ffff33';
-      if(data.DelaySeconds>300){
-        colour = 'red';
-        fillColour = '#FF0033';
-      }      
-    }
-
-  if(markers[data.VehicleRef]){
-    let historyLine = L.polyline([markers[data.VehicleRef].getLatLng(), [data.Lat, data.Long]], {
-      color: colour,
-      width: 10}).addTo(map);
-
-    if(!trails[data.VehicleRef]){
-      trails[data.VehicleRef] = [];
-    }
-    trails[data.VehicleRef].push(historyLine);
-    let opacity = 1;
-    for(var index = trails[data.VehicleRef].length - 1;index >=0; --index){
-      trails[data.VehicleRef][index].setStyle({opacity:opacity});
-      opacity -= 0.1;
-      if(opacity<=0){
-        map.removeLayer(trails[data.VehicleRef].shift());        
-      }
-    }
-
-    var newLatLng = new L.LatLng(data.Lat, data.Long);
-    markers[data.VehicleRef].setLatLng(newLatLng);
-    markers[data.VehicleRef].setStyle({
-      color: colour,
-      fillColor: fillColour});
-    markers[data.VehicleRef]._popup.setContent(popupText(data));
-  } else
-  {
-    markers[data.VehicleRef] = (L.circle([data.Lat, data.Long], {
-      color: colour,
-      fillColor: fillColour,
-      fillOpacity: 0.5,
-      radius: 30}).addTo(map)
-      .bindPopup(popupText(data)));
-    
-  }
-
-  
-  // markers[data.VehicleRef] = L.marker([data.Lat, data.Long]);
-  // console.log(markers[data.VehicleRef]);
-  // markers[data.VehicleRef].addToMap(map);
-  console.log(vehicles);
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude; 
 }
 
-function popupText(data){
-  // let now = new Date();
-  // var seconds = (new Date(now) - new Date(data.RecordedAtTime))/1000;
-  // let age = Math.round(seconds) + 's';
-  // if (seconds > 60){
-  //   age = Math.round(seconds/60) + 'm';
-  // }
-  // return data.ServiceID + ': ' + data.VehicleRef + ' ' + age
-  
-  let time = new Date(data.RecordedAtTime).toLocaleTimeString();
-  let delay = data.DelaySeconds > 60? ' (Delayed ' + data.DelaySeconds + 's)':'';
-  
-  let description = 'Bus ' + data.ServiceID + ' (' + data.VehicleRef + ')\n' 
-  if(['KPL', 'HVL', 'JVL', 'MEL', 'WRL'].includes(data.ServiceID)){
-    description = "(" + new Date(data.DepartureTime).toLocaleTimeString() + ' ' + data.OriginStopName + " -> " + data.DestinationStopName + ")\n";     
-   }
-  
-  let popup = description + time + delay;
-  console.log(popup);
-  return popup;
-}
+getLocation();
+
+var synth = window.speechSynthesis;
+
