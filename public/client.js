4db6874ad924c631ff9f969f11a503f3db1e2216
Glitch (hello-express) <none>
2019-07-31 04:28:42 +0000

    🚵🚋 Checkpoint ./public/client.js:72439/2816 ./views/index.html:72439/451

--- public/client.js
+++ public/client.js
@@ -1,8 +1,10 @@
 const findMeButton = document.querySelector('.findMe');
-findMeButton.addEventListener('click', findMe);
+// findMeButton.addEventListener('click', findMe);
 
 var x = document.getElementById("demo");
 var synth = window.speechSynthesis;
+let voices = [];
+let selectedVoice;
 
 function getLocation() {
   console.log('getLocation')
@@ -18,16 +20,46 @@ function showPosition(position) {
   "<br>Longitude: " + position.coords.longitude; 
 }
 
+
+
 function findMe(){
   getLocation();
   test();
 }
 
 
+
 function test(){
   const speech = new SpeechSynthesisUtterance("hello there - testing 1 2 3");
-  // speech.voice = voice;
+  speech.voice = voice;
   // speech.pitch = pitchInput.value;
   // speech.rate = rateInput.value;
   speechSynthesis.speak(speech);
 }
+
+$('#findMe').on('click', function(event) {
+  // event.preventDefault(); // To prevent following the link (optional)
+  findMe();
+  alert("yo")
+});
+
+
+
+
+const populateVoiceList = () => {
+  if(voices.length !== speechSynthesis.getVoices().length){
+    voices = speechSynthesis.getVoices();
+    
+    voices.map((voice, i) => {
+      if(voice.lang == 'en-GB' || voice.lang == 'en-NZ' ){
+      console.log(voice);
+      console.log(voice.name);
+        if(voice.name == 'Google UK English Female'){
+          voice = voice;
+        }
+      }
+    })
+  }
+}
+
+populateVoiceList();
