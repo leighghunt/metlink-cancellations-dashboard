badf2e3679f58e66d1ea7a343baf68ea7510be49
Glitch (hello-express) <none>
2021-07-18 23:06:04 +0000

    😚🍶 Checkpoint ./public/client.js:72439/545

--- public/client.js
+++ public/client.js
@@ -10,8 +10,22 @@ const getCancellationsListener = function() {
   console.log('getCancellationsListener')
   console.log(this.responseText)
   var data = JSON.parse(this.responseText)
+
+  
+  data.entity.forEach((elem) => {
+    
+    console.log(elem.alert.cause)
+  })
+
+
   
   data.entity.forEach((elem) => {console.log(elem.alert.cause)})
+
+  console.log("EFFECTS:")
+  console.log("========")
+
+  data.entity.forEach((elem) => {console.log(elem.alert.effect)})
+
 
   // for(var stop in data){
   //   allStops.push(data[stop]);
