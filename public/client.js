667d24c7ea3ef0f47d427d0fea2f38edded37f57
Glitch (hello-express) <none>
2021-08-17 03:07:14 +0000

    🌑⛱ Checkpoint ./public/client.js:72439/802 ./views/index.html:72439/1190

--- public/client.js
+++ public/client.js
@@ -21,9 +21,9 @@ $('#btnFilterServices').on('click', function(event) {
   serviceFilter = document.getElementById('filterServices').value
 
   if(serviceFilter==''){
-    document.getElementById('filterServices').style.display = 'block'
+    document.getElementById('servicesSummary').style.display = 'block'
   } else {
-    document.getElementById('filterServices').style.display = 'none'
+    document.getElementById('servicesSummary').style.display = 'none'
   }
 
   console.log(event)
